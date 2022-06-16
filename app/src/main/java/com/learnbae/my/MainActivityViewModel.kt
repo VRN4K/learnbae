dc9c4ebef6f10456a29165eb3036902d1272a36c
Veronika Kapitanova <brigmore.w@gmail.com>
2022-06-16 14:53:35 +0600

    [+] add bottom navigation bar layout on click listeners

--- app/src/main/java/com/learnbae/my/MainActivityViewModel.kt
+++ app/src/main/java/com/learnbae/my/MainActivityViewModel.kt
@@ -1,12 +1,15 @@
 package com.learnbae.my
 
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 import com.learnbae.my.presentation.base.BaseViewModel
 import com.learnbae.my.presentation.screens.Screens
 
-class MainActivityViewModel: BaseViewModel() {
-    fun openRootScreen(){
+class MainActivityViewModel : BaseViewModel() {
+    fun openRootScreen() {
         router.newRootScreen(Screens.getMainScreen())
     }
 
-
+    fun openFragment(screen: FragmentScreen) {
+        router.replaceScreen(screen)
+    }
 }
--- app/src/main/java/com/learnbae/my/TabContainerFragment.kt
+++
@@ -1,4 +0,0 @@
-package com.learnbae.my
-
-class TabContainerFragment {
-}
